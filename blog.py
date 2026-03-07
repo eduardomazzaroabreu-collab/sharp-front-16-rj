bc5847bc62f9153b1257eb7750e80e0d25fd45d7
eduardomazzaroabreu-collab <eduardomazzaroabreu@gmail.com>
2026-03-07 19:43:22 -0300

    Update blog.py

--- blog.py
+++ blog.py
@@ -707,7 +707,7 @@ def home():
             
             .simbolo-comunista {{
                 color: #ff0000;
-                font-size: 2.2rem;
+                font-size: 2.9rem;
                 filter: drop-shadow(0 0 5px rgba(255,0,0,0.5));
                 line-height: 1;
                 display: inline-block;
